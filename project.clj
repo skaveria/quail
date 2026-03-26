e913fee9c0d59bce0bd6caaba7e9d8c8129633a1
Nichole Kernreicht <nicholekernreicht@gmail.com>
2026-03-26 13:55:35 -0700

    added lein project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject quail "0.1.0-SNAPSHOT"
+  :description "Quail webserver"
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [ring/ring-core "1.15.0"]
+                 [ring/ring-jetty-adapter "1.15.0"]]
+  :main quail.core)
